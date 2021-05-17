afe0bba05efae03f20c3096a5b8508707ed63054
FabianCristancho <fabiancrisrincon@outlook.com>
2021-05-17 16:17:29 -0500

    Other example4

--- dist/routes/list.js
+++ dist/routes/list.js
@@ -11,7 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __asyncValues = (this && this.__asyncValues) || function (o) {
     if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
     var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    return m ? m.call(o) : (typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
     // function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
     // function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
 };
@@ -190,7 +190,6 @@ listRoutes.post("/delete", authentication_1.verifyToken, (req, res) => {
     AND l.id = ${req.body.id}`;
     connection_1.con.query(query, function (err, result) {
         var result_1, result_1_1;
-        var e_1, _a;
         return __awaiter(this, void 0, void 0, function* () {
             if (err)
                 throw err;
@@ -205,7 +204,7 @@ listRoutes.post("/delete", authentication_1.verifyToken, (req, res) => {
                     });
                 }
             }
-            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            catch (e_1_1) { console.log("error"); }
             finally {
                 console.log("error");
             }
