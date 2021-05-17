2b7dbe1bf631e3ef2ecb06d7c3c63dc5373284e0
FabianCristancho <fabiancrisrincon@outlook.com>
2021-05-17 16:01:48 -0500

    Example 2

--- dist/routes/list.js
+++ dist/routes/list.js
@@ -12,6 +12,8 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
     if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
     var m = o[Symbol.asyncIterator], i;
     return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    // function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    // function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
@@ -208,7 +210,7 @@ listRoutes.post("/delete", authentication_1.verifyToken, (req, res) => {
                 try {
                     if (result_1_1 && !result_1_1.done && (_a = result_1.return)) yield _a.call(result_1);
                 }
-                finally { if (e_1) throw e_1.error; }
+                finally { if (e_1) throw e_1; }
             }
             let sql = `DELETE FROM lists WHERE id = ${req.body.id}`;
             connection_1.con.query(sql, function (err, resul) {
